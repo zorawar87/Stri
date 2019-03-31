6ac03af2967ba99301bfb2ead37bf1e9111dddda
Zorawar Moolenaar <zorawar87@gmail.com>
2019-03-31 12:55:13 +0200

    done

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Node.js & npm --- [Available here](https://nodejs.org/en/download/) or through a
 2. `npm install ganache-cli && npm && ln -s node_modules/.bin/ganache-cli .` The npm installation will cause a lot of output. Believe it or not, it's not malware.
 3. In a separet terminal run `./ganache-cli`, and make sure it is listening on `localhost:8545` (default)
 4. Navigate to <https://remix.ethereum.org> in a browser.
+5. On the top left, click the folder icon and upload `contracts/*.sol`
+6. Click on `Manager.sol` in the left panel, and then compile using the right panel.
+7.  On the right, in "Environment", choose "Web3 Provider". Hit ok on the dialog box, and accept the default URL "https://localhost:8545". Iff any issues, make sure that ganache is running.
+8. Right below, Select "Manager" and hit "Deploy". 
+9. Paste this address in "abi.js" at contractAddress.
+10. the application *should* be ready :)
+
--- js/index.js
+++ js/index.js
@@ -23,3 +23,52 @@ function fromUNIXtime(time){
     return new Date(time*1000).toDateString();
 }
 
+milestones_buffer = new Set(); 
+
+$('#milestone_button').click(addMilestone);
+function addMilestone() {
+    let formData = new FormData(document.getElementById("milestones"));
+    if (formData.get("description") == "" && formData.get("due_date") == "") return;
+
+    milestones_buffer.add({
+        description: formData.get("description"),
+        due: toUNIXtime(formData.get("due_date"))
+    });
+
+    $(':input','#milestones')
+        .not(':button, :submit, :reset, :hidden')
+        .val('');
+
+    $('#milestones_buffer').append("<li> {" + formData.get("description") + "} is due on {"+ formData.get("due_date")+"}</li>");
+}
+
+$('#theme_submit').click(addTheme);
+function addTheme(){
+    if (milestones_buffer.size == 0) {
+        alert("no milestones => no way to get wager back");
+        return;
+    }
+    goalIds = submitMilestones();
+
+    formData = new FormData(document.getElementById("theme_form"));
+    if (formData.get("wager") == "" && formData.get("theme_title") == "") return;
+
+    gt.addTheme(formData.get("wager"), formData.get("theme_title"), goalIds, 10, tx_param);
+
+    $(':input','#theme_form')
+        .not(':button, :submit, :reset, :hidden')
+        .val('');
+}
+
+function submitMilestones(){
+    goal_ids = []
+    milestones_buffer.forEach(function(val){
+        gt.addGoal(val.description, val.due, tx_param);
+        goal_ids.push(gt.getGoalCount().toNumber());
+    }); 
+    milestones_buffer.clear();
+    $("#milestones_buffer").empty();
+    $("#addTheme").modal('hide');
+    return goal_ids;
+}
+
