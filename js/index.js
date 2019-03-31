0d256f1789494e59c262dd8891d8b0fe1286e7a6
Zorawar Moolenaar <zorawar87@gmail.com>
2019-03-31 12:48:53 +0200

    final-ish'

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-debugging = false; //true to see console messages
 _gasLimit = 300000;
-
 
 // web3js configuration
 if (typeof web3 !== 'undefined')  {
@@ -8,69 +6,20 @@ if (typeof web3 !== 'undefined')  {
 } else {
 	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
+
 sys_acc = web3.eth.accounts[0];
 usr_acc = web3.eth.defaultAccount = web3.eth.accounts[1];
 abiDecoder.addABI(abi);
-var GoalTrackerContractSpec = web3.eth.contract(abi);
-var gt = GoalTrackerContractSpec.at(contractAddress)
+var onTrackContractSpec = web3.eth.contract(abi);
+var onTrackDapp = onTrackContractSpec.at(contractAddress)
 
 tx_param = {from: usr_acc, gas: _gasLimit};
-
-milestones_buffer = new Set(); 
-
-$('#milestone_button').click(addMilestone);
-function addMilestone() {
-    let formData = new FormData(document.getElementById("milestones"));
-    if (formData.get("description") == "" && formData.get("due_date") == "") return;
-
-    milestones_buffer.add({
-        description: formData.get("description"),
-        due: toUNIXtime(formData.get("due_date"))
-    });
-
-    $(':input','#milestones')
-        .not(':button, :submit, :reset, :hidden')
-        .val('');
-
-    $('#milestones_buffer').append("<li> {" + formData.get("description") + "} is due on {"+ formData.get("due_date")+"}</li>");
-}
-
-$('#theme_submit').click(addTheme);
-function addTheme(){
-    if (milestones_buffer.size == 0) {
-        alert("no milestones => no way to get wager back");
-        return;
-    }
-    goalIds = submitMilestones();
-
-    formData = new FormData(document.getElementById("theme_form"));
-    if (formData.get("wager") == "" && formData.get("theme_title") == "") return;
-
-    gt.addTheme(formData.get("wager"), formData.get("theme_title"), goalIds, 10, tx_param);
-
-    $(':input','#theme_form')
-        .not(':button, :submit, :reset, :hidden')
-        .val('');
-}
-
-function submitMilestones(){
-    goal_ids = []
-    milestones_buffer.forEach(function(val){
-        gt.addGoal(val.description, val.due, tx_param);
-        goal_ids.push(gt.getGoalCount().toNumber());
-    }); 
-    milestones_buffer.clear();
-    $("#milestones_buffer").empty();
-    $("#addTheme").modal('hide');
-    return goal_ids;
-}
 
 function toUNIXtime(date){
     return new Date(date).getTime()/1000;
 }
 
 function fromUNIXtime(time){
-    return new Date(time*1000);
+    return new Date(time*1000).toDateString();
 }
-
 
--- js/ui.js
+++ js/ui.js
@@ -3,6 +3,33 @@ $('#due_date').datepicker({
     todayHighlight: true
 });
 
+function getThemes(){
+    nThemes = onTrackDapp.getThemeCount().toNumber();
+    for(i=0; i < nThemes; i++){
+        currentTheme = onTrackDapp.themes(usr_acc, i);
+        goalIds = onTrackDapp.getGoals(usr_acc, currentTheme[0].toNumber()-1);
+
+        goalData =[]
+        goalIds.forEach(function(val){
+            goalData.push(onTrackDapp.goals(val.toNumber()));
+        });
+
+        goals = []
+        goalData.forEach(function(val){
+            goals.push({
+                description: val[1],
+                date: fromUNIXtime(val[2].toNumber())
+            })
+        });
+
+        populateThemes(
+            currentTheme[2], currentTheme[1].toNumber(),
+            goals
+        );
+    }
+}
+getThemes();
+
 function populateThemes(themeName, wager, goals){
 newTheme = '<div class="col-md-3 panelspace"><div class="panel panel-default"><div class="panel-heading">'
          + themeName + '<span class="badge badge-pill badge progress-bar-dark"> €' 
@@ -26,32 +53,77 @@ return '<li class="list-group-item"><div class="card" style="width: 18rem;"><div
                 + '</button></div></li>';
 }
 
-function getThemes(){
-    ALL = []
-    nThemes = gt.getThemeCount().toNumber();
-    for(i=0; i < nThemes; i++){
-        currentTheme = gt.themes(usr_acc, i);
-        goalIds = gt.getGoals(usr_acc, currentTheme[0].toNumber()-1);
-        ALL.push(currentTheme);
-
-        goalData =[]
-        goalIds.forEach(function(val){
-            goalData.push(gt.goals(val.toNumber()));
-        });
-
-        goals = []
-        goalData.forEach(function(val){
-            goals.push({
-                description: val[1],
-                date: fromUNIXtime(val[2].toNumber())
-            })
-        });
-        ALL.push(goals);
-
-        populateThemes(
-            currentTheme[2], currentTheme[1].toNumber(),
-            goals
-        );
+milestones_buffer = new Set(); 
+$('#milestone_button').click(function () {
+    formData = new FormData(document.getElementById("milestones"));
+    if (formData.get("description") == "" && formData.get("due_date") == "") {
+        alert("please enter a valid milestone!")
+        return;
     }
-    return ALL;
+
+    milestones_buffer.add({
+        description: formData.get("description"),
+        due: toUNIXtime(formData.get("due_date"))
+    });
+
+    $(':input','#milestones')
+        .not(':button, :submit, :reset, :hidden')
+        .val('');
+
+    $('#milestones_buffer').append("<li> {" + formData.get("description") + "} is due on {"+ formData.get("due_date")+"}</li>");
+});
+
+proof = -1;
+$("#url_button").click(function(){
+    if($("#url").val() == 0)
+        alert("enter proof url!")
+    proof++;
+
+    onTrackDapp.addProof(proof, $("#url").val());
+});
+
+$('#theme_submit').click(function (){
+    if (milestones_buffer.size == 0) {
+        alert("without milestones, you can't get your wager back");
+        return;
+    }
+    goalIds = submitMilestones();
+
+    formData = new FormData(document.getElementById("theme_form"));
+    if (formData.get("wager") == "" && formData.get("theme_title") == "") return;
+
+    onTrackDapp.addTheme(formData.get("wager"), formData.get("theme_title"), goalIds, 10, tx_param);
+
+    $(':input','#theme_form')
+        .not(':button, :submit, :reset, :hidden')
+        .val('');
+});
+
+function submitMilestones(){
+    goal_ids = []
+    milestones_buffer.forEach(function(val){
+        onTrackDapp.addGoal(val.description, val.due, tx_param);
+        goal_ids.push(onTrackDapp.getGoalCount().toNumber());
+    }); 
+    milestones_buffer.clear();
+    $("#milestones_buffer").empty();
+    $("#addTheme").modal('hide');
+    return goal_ids;
+}
+
+
+function addSampleToDapp(){
+    goal_ids = []
+    onTrackDapp.addGoal("Look for accommodations", toUNIXtime("01/05/19"), tx_param);
+    goal_ids.push(onTrackDapp.getGoalCount().toNumber());
+    onTrackDapp.addGoal("Look for flight tickets", toUNIXtime("15/05/19"), tx_param);
+    goal_ids.push(onTrackDapp.getGoalCount().toNumber());
+    onTrackDapp.addTheme(150, "Plan trip to Japan", goal_ids, 10, tx_param);
+
+    goal_ids = []
+    onTrackDapp.addGoal("Learn 20 new words in a week", toUNIXtime("12/04/19"), tx_param);
+    goal_ids.push(onTrackDapp.getGoalCount().toNumber());
+    onTrackDapp.addGoal("Learn 3 conjugations", toUNIXtime("20/04/19"), tx_param);
+    goal_ids.push(onTrackDapp.getGoalCount().toNumber());
+    onTrackDapp.addTheme(20, "Learning french", goal_ids, 10, tx_param);
 }
